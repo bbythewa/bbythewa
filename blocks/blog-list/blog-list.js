1bff9a80b296b9c092abeb9b33197026d3e24a91
Benjamin Bytheway <bbythewa@adobe.com>
2023-06-15 11:46:17 -0600

    blog-list block

--- blocks/blog-list/blog-list.js
+++ blocks/blog-list/blog-list.js
@@ -8,7 +8,7 @@ export default async (el) => {
     return;
   }
 
-  const list = createTag('ul', {
+  const ul = createTag('ul', {
     class: 'blog-list-container'
   });
 
@@ -25,10 +25,12 @@ export default async (el) => {
       const doc = parser.parseFromString(html, 'text/html');
       const heading =- doc.querySelector('h1, h2, h3, h4, h5, p');
       const li = createTag('li', { class: 'blog-list-item'}, heading);
-      list.append(li);
+      ul.append(li);
 
       res();
     }); 
 
   }));
+
+  el.append(li);
 }
