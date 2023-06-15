9c6bd51f510b17c8afd908b93eb208527cf16406
Benjamin Bytheway <bbythewa@adobe.com>
2023-06-15 11:28:41 -0600

    blog-list block

---
+++ blocks/block-list/block-list.js
@@ -0,0 +1,7 @@
+import { getLibs } from '../../scripts/utils.js';
+
+export default async (el) => {
+  // const { createTag, loadScript } = await import(`${getLibs()}/utils/utils.js`);
+  
+  console.log(el);
+}
