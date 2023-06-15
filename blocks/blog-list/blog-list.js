caaa2ce6b789775fa2381e4944d7117f821e3a2d
Benjamin Bytheway <bbythewa@adobe.com>
2023-06-15 11:40:56 -0600

    blog-list block

--- blocks/blog-list/blog-list.js
+++ blocks/blog-list/blog-list.js
@@ -1,7 +1,34 @@
 import { getLibs } from '../../scripts/utils.js';
 
 export default async (el) => {
-  // const { createTag, loadScript } = await import(`${getLibs()}/utils/utils.js`);
-  
-  console.log(el);
+  const { createTag, loadScript } = await import(`${getLibs()}/utils/utils.js`);
+  const r = await fetch('/blog/query-index.json');
+
+  if (!r.ok) {
+    return;
+  }
+
+  const list = createTag('ul', {
+    class: 'blog-list-container'
+  });
+
+  r = await r.json();
+
+  await Promise.all(r.data.map((p) => {
+
+    return new Promise(async (res) => {
+      
+      const r = await fetch(`${p.path}.plain.html`);
+
+      const html = await r.text();
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(html, 'text/html');
+      const heading =- doc.querySelector('h1, h2, h3, h4, h5, p');
+      const li = createTag('li', { class: 'blog-list-item'}, heading);
+      list.append(li);
+      
+      res();
+    }); 
+
+  }));
 }
